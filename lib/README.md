44857e7378631c183554db14af58581fc503f19a
Eldath <lizhaohan001@live.cn>
2016-07-24 15:39:59 +0800

    Rewrite .gitgnore.

---
+++ lib/README.md
@@ -0,0 +1,22 @@
+﻿# Where is my dependences??
+
+---
+In order to minimize the size of the project, please download dependent files on [FileFactory][1], [ShareSend][2] ,[BaiduYun][3] or [360Yun (Password: 3211)][4], Then unzip files and put them in the \lib directory of the project.
+We are very sorry for the inconvenience. 
+——Castle Game Team
+
+---
+
+为了尽量缩减项目大小，依赖文件请移步至 [FileFactory, [ShareSend][5], [百度云][6] 或 [360云盘（提取码：3211）][7] 处下载。然后将解压后的文件放置在lib文件夹中。
+对于给您带来的不便，我们非常抱歉。
+——城堡游戏团队
+
+
+  [1]: http://www.filefactory.com/file/57hfyecdyks7/lib.7z
+  [2]: http://sharesend.com/3melmlnc
+  [3]: https://pan.baidu.com/s/1sljCirr
+  [4]: https://yunpan.cn/c6sITGIGxDmHT
+  [5]: http://sharesend.com/3melmlnc
+  [6]: https://pan.baidu.com/s/1sljCirr
+  [74]: https://pan.baidu.com/s/1sljCirr
+  [5]: https://yunpan.cn/c6sITGIGxDmHT
--- src/view/GUI.java
+++ src/view/GUI.java
@@ -7,110 +7,142 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
+import java.io.FileInputStream;
 
 /**
  * 视图
  * Created by asus1 on 2016/1/31.
  */
 public class GUI extends Game
-		implements Echoer {
+        implements Echoer {
 
-	private JTextField textField;
-	private JTextArea textArea;
-	private JFrame frame;
-	private JScrollPane scrollPane;
+    private JTextField textField;
+    private JTextArea textArea;
+    private JFrame frame;
+    private JScrollPane scrollPane;
 
-	private static int FRAME_X = 500;
-	private static int FRAME_Y = 500;
-	private static int INPUT_Y = 500;
+    private static int FRAME_X = 500;
+    private static int FRAME_Y = 500;
+    private static int INPUT_Y = 500;
 
-	public GUI() {
-		frame = new JFrame("城堡游戏   by 千里冰封");
-		textField = new JTextField("在这里输入指令");
-		textField.registerKeyboardAction(e -> {
-					HandleMessage(textField.getText());
-					textField.setText("");
-				},
-				KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, true),
-				JComponent.WHEN_FOCUSED
-		);
-		textArea = new JTextArea();
-		textArea.addMouseListener(new MouseListener() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				echoln("别点这里，点下面。");
-			}
+    private static Font loadFont(String fontFileName, float fontSize)  //第一个参数是外部字体名，第二个是字体大小
+    {
+        try {
+            File file = new File(fontFileName);
+            FileInputStream font = new FileInputStream(file);
+            Font dynamicFont = Font.createFont(Font.TRUETYPE_FONT, font);
+            Font dynamicFontPt = dynamicFont.deriveFont(fontSize);
+            font.close();
+            return dynamicFontPt;
+        } catch (Exception e)//异常处理
+        {
+            e.printStackTrace();//TODO 或许。。不这样异常处理？？！！
+            return new java.awt.Font("宋体", Font.PLAIN, 14);
+        }
+    }
 
-			@Override
-			public void mousePressed(MouseEvent e) {
-			}
+    public GUI() {
+        frame = new JFrame("城堡游戏   by 千里冰封");
+        String note = "在这里输入指令";
+        textField = new JTextField(note);
+        textField.registerKeyboardAction(e -> {
+                    HandleMessage(textField.getText());
+                    textField.setText("");
+                },
+                KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, true),
+                JComponent.WHEN_FOCUSED
+        );
+        textField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                textField.setText("");
+            }
 
-			@Override
-			public void mouseReleased(MouseEvent e) {
+            @Override
+            public void focusLost(FocusEvent e) {
+                textField.setText(note);
+            }
+        });
+        textArea = new JTextArea();
+        Font font=loadFont(System.getProperty("user.dir") + "/lib/MSYHMONO.ttf", 13f);
+        textArea.setFont(font);
+        textField.setFont(font);
+        textArea.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                echoln("别点这里，点下面。");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
 //				echoln("对，放开就好。");
-			}
+            }
 
-			@Override
-			public void mouseEntered(MouseEvent e) {
-			}
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
 
-			@Override
-			public void mouseExited(MouseEvent e) {
-			}
-		});
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+        });
 
-		/// IntelliJ IDEA 标准配色
-		textArea.setBackground(new Color(43, 43, 43));
-		textArea.setForeground(new Color(169, 183, 198));
-		textArea.setEditable(false);
-		frame.setIconImage(Toolkit.getDefaultToolkit().createImage(
-				"." + File.separator + "src" + File.separator + "drawable" + File.separator + "ic_launcher.png"
-		));
-		frame.setSize(FRAME_X, FRAME_Y);
-		// 绝对布局
-		// frame.setLayout(null);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.add(textField, BorderLayout.SOUTH);
-		scrollPane = new JScrollPane(textArea);
-		JScrollBar scrollBar = scrollPane.getVerticalScrollBar();
-		frame.add(scrollPane, BorderLayout.CENTER);
-		frame.setResizable(false);
-		frame.setLocation(FRAME_X / 8, FRAME_Y / 8);
-		frame.setVisible(true);
-	}
+        /// IntelliJ IDEA 标准配色
+        textArea.setBackground(new Color(43, 43, 43));
+        textArea.setForeground(new Color(169, 183, 198));
+        textArea.setEditable(false);
+        frame.setIconImage(Toolkit.getDefaultToolkit().createImage(
+                "." + File.separator + "src" + File.separator + "drawable" + File.separator + "ic_launcher.png"
+        ));
+        frame.setSize(FRAME_X, FRAME_Y);
+        // 绝对布局
+        // frame.setLayout(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(textField, BorderLayout.SOUTH);
+        scrollPane = new JScrollPane(textArea);
+        JScrollBar scrollBar = scrollPane.getVerticalScrollBar();
+        frame.add(scrollPane, BorderLayout.CENTER);
+        frame.setResizable(false);
+        frame.setLocation(FRAME_X / 8, FRAME_Y / 8);
+        frame.setVisible(true);
+    }
 
-	public static void main(String[] args) {
-		GUI game = new GUI();
-		game.onStart();
-	}
+    public static void main(String[] args) {
+        GUI game = new GUI();
+        game.onStart();
+    }
 
-	@Override
-	public void echo(String words) {
+    @Override
+    public void echo(String words) {
 //		System.out.print(words);
-		textArea.append(words);
-		int i = textArea.getText().length();
-		int MAX_LENGTH = 10000;
-		if (i > MAX_LENGTH) {
-			textArea.setText(textArea.getText().substring(
-					i - MAX_LENGTH, i
-			));
-		}
+        textArea.append(words);
+        int i = textArea.getText().length();
+        int MAX_LENGTH = 10000;
+        if (i > MAX_LENGTH) {
+            textArea.setText(textArea.getText().substring(
+                    i - MAX_LENGTH, i
+            ));
+        }
 //		scrollBar.setValue(scrollBar.getMaximum() - 20);
-		// 滚动到最底下
-		int height = 10;
-		Point p = new Point();
-		p.setLocation(0, this.textArea.getLineCount() * height);
-		this.scrollPane.getViewport().setViewPosition(p);
-	}
+        // 滚动到最底下
+        int height = 10;
+        Point p = new Point();
+        p.setLocation(0, this.textArea.getLineCount() * height);
+        this.scrollPane.getViewport().setViewPosition(p);
+    }
 
-	@Override
-	public void echoln(String words) {
-		echo(words + "\n");
-	}
+    @Override
+    public void echoln(String words) {
+        echo(words + "\n");
+    }
 
-	@Override
-	public void closeScreen() {
-		frame.dispose();
-	}
+    @Override
+    public void closeScreen() {
+        frame.dispose();
+    }
 
 }
