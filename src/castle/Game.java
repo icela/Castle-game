0cccdb82d8edd967543c9981f9631f9fda1d0c92
Eldath <lizhaohan001@live.cn>
2016-07-25 23:18:04 +0800

    Fixed lots of ** problem...

--- src/castle/Game.java
+++ src/castle/Game.java
@@ -184,44 +184,31 @@ public abstract class Game
 			JOptionPane.showMessageDialog(null, "保存失败", "请在随后弹出的对话框中选择 “是”！", JOptionPane.ERROR_MESSAGE);
 			try {
 				//TODO 记得加上 管理员权限获取器。
-				File file = new File("//temp//uac.bat");
+				File file = new File("\temp\uac.bat");
 				file.createNewFile();
-				String data = ":: BatchGotAdmin\n" +
-						":-------------------------------------\n" +
+				String data = ":: BatchGotAdmin+"\n" +
+						":-------------------------------------+"\n" +
 						"REM --> Check for permissions" + "\n" +
-						">nul 2>&1\n" + "%SYSTEMROOT%\\system32\\cacls.exe\n"
-						+ "%SYSTEMROOT%\\system32\\config\\system\n" +
-						"REM --> If error flag set, we do not have admin.\n" +
-				if '%errorlevel%' NEQ '0' (
-						echo Requesting administrative privileges...
-				"+"\n"+
-				"goto UACPrompt" +
-						")"+"\n"+
+						">nul 2>&1+"\n" + 
+						"%SYSTEMROOT%\\system32\\cacls.exe+"\n"+ 
+						"%SYSTEMROOT%\\system32\\config\\system+"\n" +
+						"REM --> If error flag set, we do not have admin.+"\n" +
+				"if '%errorlevel%' NEQ '0' ("+"\n"+
+						"echo Requesting administrative privileges..."+"\n"+
+						"goto UACPrompt" +")"+"\n"+
 				"else ( goto gotAdmin )" + "\n" +
-						"
-:UACPrompt "+"\n"+
-				"
-				echo Set UAC = CreateObject ^ ("Shell.Application" ^) > "%temp%\getadmin.vbs" "+"\n"+
-				"
-				echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-				"%temp%\getadmin.vbs" "+"\n"+
-				"
-				exit / B "+"\n+
-
-						":gotAdmin" + "\n+
-				"
-				if exist "%temp%\getadmin.vbs" (del "%temp%\getadmin.vbs" )"+"\n"+
-				"
-				pushd "%CD%" "+"\n"+
-				"
-				CD / D "%~dp0" "+"\n+
-						"
-:--------------------------------------";";
+						":UACPrompt "+"\n"+
+				"echo Set UAC = CreateObject ^ (\"Shell.Application\" ^) > \"%temp%\getadmin.vbs" "+"\n"+
+				"echo UAC.ShellExecute \"%~s0\", \"\", \"\", \"runas\", 1 >> \"%temp%\getadmin.vbs\" \"%temp%\getadmin.vbs\\" "+"\n"+
+				"exit / B "+"\n"+":gotAdmin" + "\n"+"if exist \"%temp%\getadmin.vbs\" (del \"%temp%\getadmin.vbs\" )"+"\n"+
+				"pushd \"%CD%\" "+"\n"+
+				"CD / D \"%~dp0\" "+"\n"+
+				":--------------------------------------";
 				FileWriter fw = new FileWriter(file.getName());
 				fw.write(data);
 				Process ps = Runtime.getRuntime().exec();
 
-				InputStream in = ps.getInputStream("//temp//uac.bat");
+				InputStream in = ps.getInputStream("\temp\uac.bat");
 
 				intc;
 
