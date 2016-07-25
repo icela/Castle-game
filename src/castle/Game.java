b260fd57f996c643c0e774e8e447310f05984d48
Eldath <lizhaohan001@live.cn>
2016-07-25 22:33:11 +0800

    Add UAC system.

--- src/castle/Game.java
+++ src/castle/Game.java
@@ -69,7 +69,7 @@ public abstract class Game
 			if (npc != null) {
 				echoln(npc.getChat());
 			} else
-				echoln("你指定的名字不存在。注：Boss要在被打败之后才能对话。");
+				echoln("指定的名字不存在。注：Boss要在被打败之后才能对话。");
 		});
 		commands.put(commandNames[10], cmd -> {
 			echoln("背包中物品如下：");
@@ -117,8 +117,8 @@ public abstract class Game
 		}
 
 		echoln("您好，" + player);
-		echoln("如果您需要任何帮助，请输入 'help' 。\n");
-		echo("现在");
+		echoln("如果您需要任何帮助，请键入 'help' 并回车。\n");
+		echo("您的位置：");
 		echoln(map.currentRoom.getPrompt());
 	}
 
@@ -140,7 +140,7 @@ public abstract class Game
 				return false;
 			}
 		} else
-			echoln("对不起，输入指令错误！");
+			echoln("对不起，输入指令有误！");
 		return true;
 	}
 
@@ -176,7 +176,64 @@ public abstract class Game
 			TextDatabase.getInstance().saveMapAndState(map, player);
 			echoln("保存成功。");
 		} catch (IOException e) {
-			echoln("保存失败，请检查是否有管理员权限！");
+			JOptionPane.showMessageDialog(null, "保存失败", "请在随后弹出的对话框中选择 “是”！", JOptionPane.ERROR_MESSAGE); 
+			try {
+			//TODO 记得加上 管理员权限获取器。
+			File file=new File("//temp//uac.bat");
+			file.createNewFile();
+			String data=":: BatchGotAdmin"+"/n"+
+			":-------------------------------------"+"/n" +
+
+		    "REM --> Check for permissions"+"/n"+
+			">nul 2>&1"+"/n"
+			+ ""%SYSTEMROOT%\system32\cacls.exe""+"/n"
+			+ ""%SYSTEMROOT%\system32\config\system""
++"/n"+
+			"REM --> If error flag set, we do not have admin."+"/n"+
+			"
+if '%errorlevel%' NEQ '0' ( 
+echo Requesting administrative privileges... 
+"+"/n"+
+			"goto UACPrompt 
+) "+"/n"+
+			"else ( goto gotAdmin )"+"/n"+
+		  	"
+:UACPrompt "+"/n"+
+			"
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs""+"/n"+
+			"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs" 
+"%temp%\getadmin.vbs""+"/n"+
+			"
+exit /B"+"/n+
+		   	
+":gotAdmin"+"/n+ 
+			"
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )"+"/n"+
+			"
+pushd "%CD%""+"/n"+
+			"
+CD /D "%~dp0""+"/n+
+			"
+:--------------------------------------";";
+			FileWriter fw=new FileWriter(file.getName());
+			fw.write(data);
+			Process ps = Runtime.getRuntime().exec();
+
+			InputStream in = ps.getInputStream("//temp//uac.bat");
+
+			intc;
+			
+while((c = in.read()) != -1) 
+				{
+				}
+				
+in.close
+				();
+ps.waitFor();
+			}catch (Exception e){
+				//TODO 异常处理
+			}
 		}
 	}
 
