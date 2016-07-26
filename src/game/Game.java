f59bad197fc99649a31961aeb5b667e0f65b7d60
Eldath <lizhaohan001@live.cn>
2016-07-26 15:35:12 +0800

    Added command: clear.

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,10 +5,7 @@ import game.cells.Item;
 import game.cells.NPC;
 import game.cells.Player;
 import game.commands.BaseCommand;
-import game.commands.implement.CommandMap;
-import game.commands.implement.CommandPick;
-import game.commands.implement.CommandSleep;
-import game.commands.implement.CommandUse;
+import game.commands.implement.*;
 import game.map.GameMap;
 import game.map.Room;
 import util.AdminErrorHandler;
@@ -20,156 +17,157 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public abstract class Game
-		implements MessageHandler, Echoer {
-	private HashMap<String, BaseCommand> commands = new HashMap<>();
-	private String[] commandNames;
-	private GameMap map;
-	private ArrayList<Item> items = new ArrayList<>();
-	public Player player;
+        implements MessageHandler, Echoer {
+    private HashMap<String, BaseCommand> commands = new HashMap<>();
+    private String[] commandNames;
+    private GameMap map;
+    private ArrayList<Item> items = new ArrayList<>();
+    public Player player;
 
-	private boolean gameEnded = false;
+    private boolean gameEnded = false;
 
-	//    构造方法
-	public Game() {
-		onCreate();
-	}
+    //    构造方法
+    public Game() {
+        onCreate();
+    }
 
-	protected void onCreate() {
-		map = new GameMap();
-		initItems();
-		commandNames = new String[]{
-				"help", "go", "wild",
-				"exit", "state", "fight",
-				"sleep", "save", "rename",
-				"talk", "pack", "home",
-				"map", "pick", "use"
-		};
+    protected void onCreate() {
+        map = new GameMap();
+        initItems();
+        commandNames = new String[]{
+                "help", "go", "wild",
+                "exit", "state", "fight",
+                "sleep", "save", "rename",
+                "talk", "pack", "home",
+                "map", "pick", "use", "reset"
+        };
 
-		int index = -1;
-		commands.put(commandNames[++index], cmd -> {
-			for (String s : commandNames)
-				echoln(s);
-			echoln("有些需要参数的命令请按如下格式输入：\n命令 [参数]\n如：go east");
-			echoln("如：rename 冰封");
-		});
-		commands.put(commandNames[++index], this::goRoom);
-		commands.put(commandNames[++index], cmd -> echoln(map.wildRoom()));
-		commands.put(commandNames[++index], cmd -> {
-			saveData();
-			gameEnded = true;
-		});
-		commands.put(commandNames[++index], cmd -> echoln(player.stateToString()));
-		commands.put(commandNames[++index], cmd -> fight());
-		commands.put(commandNames[++index], new CommandSleep(this));
-		commands.put(commandNames[++index], cmd -> saveData());
-		commands.put(commandNames[++index], cmd -> {
-			if (!cmd.equals("")) {
-				player.rename(cmd);
-				echoln("重命名成功。新名字：" + cmd);
-			} else
-				echoln("格式错误。请按照\"rename [新名字]\"的格式重命名！");
-		});
-		commands.put(commandNames[++index], cmd -> {
-			NPC npc = map.currentRoom.isNPCExists(cmd);
-			if (npc != null) {
-				echoln(npc.getChat());
-			} else
-				echoln("指定的名字不存在。注：Boss要在被打败之后才能对话。");
-		});
-		commands.put(commandNames[++index], cmd -> {
-			echoln("背包中物品如下：");
-			for (Item item : items)
-				echoln(item.toString());
-		});
-		commands.put(commandNames[++index], cmd -> {
-			echoln("您发动了与女仆的契约，回到了旅馆。");
-			map.currentRoom = map.getHome();
-			echoln(map.currentRoom.getPrompt());
-		});
-		commands.put(commandNames[++index], new CommandMap(this));
-		commands.put(commandNames[++index], new CommandPick(this));
-		commands.put(commandNames[++index], new CommandUse(this));
-	}
+        int index = -1;
+        commands.put(commandNames[++index], cmd -> {
+            for (String s : commandNames)
+                echoln(s);
+            echoln("有些需要参数的命令请按如下格式输入：\n命令 [参数]\n如：go east");
+            echoln("如：rename 冰封");
+        });
+        commands.put(commandNames[++index], this::goRoom);
+        commands.put(commandNames[++index], cmd -> echoln(map.wildRoom()));
+        commands.put(commandNames[++index], cmd -> {
+            saveData();
+            gameEnded = true;
+        });
+        commands.put(commandNames[++index], cmd -> echoln(player.stateToString()));
+        commands.put(commandNames[++index], cmd -> fight());
+        commands.put(commandNames[++index], new CommandSleep(this));
+        commands.put(commandNames[++index], cmd -> saveData());
+        commands.put(commandNames[++index], cmd -> {
+            if (!cmd.equals("")) {
+                player.rename(cmd);
+                echoln("重命名成功。新名字：" + cmd);
+            } else
+                echoln("格式错误。请按照\"rename [新名字]\"的格式重命名！");
+        });
+        commands.put(commandNames[++index], cmd -> {
+            NPC npc = map.currentRoom.isNPCExists(cmd);
+            if (npc != null) {
+                echoln(npc.getChat());
+            } else
+                echoln("指定的名字不存在。注：Boss要在被打败之后才能对话。");
+        });
+        commands.put(commandNames[++index], cmd -> {
+            echoln("背包中物品如下：");
+            for (Item item : items)
+                echoln(item.toString());
+        });
+        commands.put(commandNames[++index], cmd -> {
+            echoln("您发动了与女仆的契约，回到了旅馆。");
+            map.currentRoom = map.getHome();
+            echoln(map.currentRoom.getPrompt());
+        });
+        commands.put(commandNames[++index], new CommandMap(this));
+        commands.put(commandNames[++index], new CommandPick(this));
+        commands.put(commandNames[++index], new CommandUse(this));
+        commands.put(commandNames[++index], new CommandReset(this));
+    }
 
-	protected void onStart() {
-		echoln("欢迎来到Castle Game！");
-		echoln("这是一个超复古的CUI游戏。");
-		echoln("最新版本和源代码请见https://github.com/ProgramLeague/Castle-game");
-		echoln("Kotlin版本与旧版本请见https://github.com/ice1000/Castle-game");
-		echoln("敬请期待OL版本https://github.com/ProgramLeague/Castle-Online");
+    protected void onStart() {
+        echoln("欢迎来到Castle Game！");
+        echoln("这是一个超复古的CUI游戏。");
+        echoln("最新版本和源代码请见https://github.com/ProgramLeague/Castle-game");
+        echoln("Kotlin版本与旧版本请见https://github.com/ice1000/Castle-game");
+        echoln("敬请期待OL版本https://github.com/ProgramLeague/Castle-Online");
 //		太羞耻了！！
 //		echoln("不过在经过了冰封的改造后，你会觉得这个很有意思。");
-		player = TextDatabase.getInstance().loadPlayer();
-		map = TextDatabase.getInstance().loadMap("宾馆");
-		if (TextDatabase.fileExists()) {
-			echoln("检测到存档。");
-		} else {
-			echoln("您是第一次开始游戏。");
-			echoln("您可以稍后使用\"rename [新名字]\"命令来更改自己的名字。");
-			saveData();
-		}
+        player = TextDatabase.getInstance().loadPlayer();
+        map = TextDatabase.getInstance().loadMap("宾馆");
+        if (TextDatabase.fileExists()) {
+            echoln("检测到存档。");
+        } else {
+            echoln("您是第一次开始游戏。");
+            echoln("您可以稍后使用\"rename [新名字]\"命令来更改自己的名字。");
+            saveData();
+        }
 
-		echoln("您好，" + player);
-		echoln("如果您需要任何帮助，请键入 'help' 并回车。\n");
-		echoln(map.currentRoom.getPrompt());
-	}
+        echoln("您好，" + player);
+        echoln("如果您需要任何帮助，请键入 'help' 并回车。\n");
+        echoln(map.currentRoom.getPrompt());
+    }
 
-	@Override
-	public boolean handleMessage(String line) {
-		String[] words = line.split(" ");
-		BaseCommand func = commands.get(words[0]);
-		String value2 = "";
+    @Override
+    public boolean handleMessage(String line) {
+        String[] words = line.split(" ");
+        BaseCommand func = commands.get(words[0]);
+        String value2 = "";
 
-		if (words.length > 1)
-			value2 = words[1];
+        if (words.length > 1)
+            value2 = words[1];
 
 //			如果找到了该指令
-		if (func != null) {
-			func.runCommend(value2);
-			if (gameEnded) {
-				saveData();
-				closeScreen();
-				return false;
-			}
-		} else
-			echoln("对不起，输入指令有误！");
-		return true;
-	}
+        if (func != null) {
+            func.runCommend(value2);
+            if (gameEnded) {
+                saveData();
+                closeScreen();
+                return false;
+            }
+        } else
+            echoln("对不起，输入指令有误！");
+        return true;
+    }
 
-	private void initItems() {
-		items.add(new Item("传送宝石"));
-		items.add(new Item("和女仆的契约"));
-	}
+    private void initItems() {
+        items.add(new Item("传送宝石"));
+        items.add(new Item("和女仆的契约"));
+    }
 
-	/**
-	 * 去一个房间
-	 */
-	public void goRoom(String direction) {
-		if (map.goRoom(direction))
-			echoln(map.currentRoom.getPrompt());
-		else
-			echoln("命令格式错误或该出口不存在。");
-	}
+    /**
+     * 去一个房间
+     */
+    public void goRoom(String direction) {
+        if (map.goRoom(direction))
+            echoln(map.currentRoom.getPrompt());
+        else
+            echoln("命令格式错误或该出口不存在。");
+    }
 
-	/**
-	 * 战斗函数
-	 */
-	public void fight() {
-		map.fightBoss(this);
-		echoln(map.currentRoom.getPrompt());
-	}
+    /**
+     * 战斗函数
+     */
+    public void fight() {
+        map.fightBoss(this);
+        echoln(map.currentRoom.getPrompt());
+    }
 
-	public Room getCurrentRoom() {
-		return map.currentRoom;
-	}
+    public Room getCurrentRoom() {
+        return map.currentRoom;
+    }
 
-	public void saveData() {
-		try {
-			TextDatabase.getInstance().saveFile(map, player);
-			echoln("保存成功。");
-		} catch (IOException e) {
-			AdminErrorHandler.handleError();
-		}
-	}
+    public void saveData() {
+        try {
+            TextDatabase.getInstance().saveFile(map, player);
+            echoln("保存成功。");
+        } catch (IOException e) {
+            AdminErrorHandler.handleError();
+        }
+    }
 
 }
--- src/game/commands/implement/CommandReset.java
+++ src/game/commands/implement/CommandReset.java
@@ -1,14 +1,41 @@
 package game.commands.implement;
 
+import game.Game;
 import game.commands.BaseCommand;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by Eldath on 2016/7/26 0026.
+ *
  * @author Eldath
  */
 public class CommandReset implements BaseCommand {
+
+    private Game game;
+
+    public CommandReset(Game game) {
+        this.game = game;
+    }
+
     @Override
     public void runCommend(String cmd) {
-
+        File archive = new File(System.getProperty("user.dir") + "\\save.ice");
+        archive.delete();
+        if (!archive.exists())
+            game.echoln("存档删除成功。");
+        else {
+            game.echoln("存档删除失败，尝试清空存档。");
+            try {
+                FileWriter fw = new FileWriter(archive.getName(), false);
+                fw.write("");
+                fw.close();
+            } catch (IOException e) {
+                //TODO 异常处理。
+            }
+            //TODO 清屏？？
+        }
     }
 }
