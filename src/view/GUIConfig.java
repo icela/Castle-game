d1872afaa5eaa396691ea0c9fdcc4258cb05fecd
ice1000 <ice1000kotlin@foxmail.com>
2016-07-27 17:34:18 +0800

    version

--- src/view/GUIConfig.java
+++ src/view/GUIConfig.java
@@ -6,7 +6,7 @@ package view;
  */
 public class GUIConfig {
 
-	public static final String VERSION = "V0.0.1 Alpha";
+	public static final String VERSION = "V0.0.2 Alpha";
 
 	public static final String GUI_FORM_TITLE = "城堡游戏   by 冰封 and Eldath" + " " + VERSION;
 
