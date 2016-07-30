c0740aef387095f4ebc524d8c205d4ba7b020536
Eldath <lizhaohan001@live.cn>
2016-07-30 17:31:08 +0800

    待我吃完饭回来解决这个bug... ...

--- src/util/error/Logger.java
+++ src/util/error/Logger.java
@@ -11,59 +11,58 @@ import java.time.LocalDateTime;
  */
 public class Logger {
 
-	private static Logger logger;
-	private static final String template = "log" + File.separator + "error%d.log";
-	private File file;
-	
-	//TODO 不太懂你啥意图。。难道直接这么写不行??
-	public Logger() {
-		if (logger == null)
-			 logger = new logger();
-		return logger;
-	}
+    private static final Logger logger=new Logger();
+    private static final String template = "log" + File.separator + "error%d.log";
+    private static File file;
+    private static FileWriter writer;
+    //TODO 待我吃完饭回来解决这个bug... ...
 
-	/* public static Logger getInstance() {
-		if (logger == null)
-			logger = new Logger();
-		return logger;
-	}
-	*/
+    public Logger() {
+        try {
+            for (int i = 0; ; i++) {
+                file = new File(String.format(template, i));
+                if (!file.exists()) {
+                    file.createNewFile();
+                    break;
+                }
+                writer = new FileWriter(file);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void log(String log) {
-		try {
-			init();
-			new FileWriter(file)
-					.append(LocalDateTime.now().toString())
-					.append('\n')
-					.append(log)
-					.append('\n')
-					.close();
-		} catch (IOException ignored) {
-		}
-	}
 
-	public void log(Exception e) {
-		try {
-			init();
-			FileWriter writer = new FileWriter(file);
-			writer.append(LocalDateTime.now().toString()).append('\n');
-			for (StackTraceElement element : e.getStackTrace())
-				writer.append(element.toString()).append('\n');
-			writer.flush();
-			writer.close();
-		} catch (IOException ignored) {
-		}
-	}
+    private static class LoggerLoader {
+        private static final Logger instance = new Logger();
+    }
 
-	private void init() throws IOException {
-		String fileName;
-		for (int i = 0; ; i++) {
-			fileName = String.format(template, i);
-			file = new File(fileName);
-			if (!file.exists()) {
-				file.createNewFile();
-				break;
-			}
-		}
-	}
+    public static Logger getInstance() {
+        return LoggerLoader.instance;
+    }
+
+    //不知道为啥，Sun说这是单例的标准写法。。。
+
+    public static void log(String log) {
+        try {
+            writer
+                    .append(LocalDateTime.now().toString())
+                    .append('\n')
+                    .append(log)
+                    .append('\n')
+                    .close();
+        } catch (IOException ignored) {
+        }
+    }
+
+    public static void log(Exception e) {
+        try {
+            writer.append(LocalDateTime.now().toString()).append('\n');
+            for (StackTraceElement element : e.getStackTrace())
+                writer.append(element.toString()).append('\n');
+            writer.flush();
+            writer.close();
+        } catch (IOException ignored) {
+        }
+    }
 }
---
+++ test/LogSystem.java
@@ -0,0 +1,10 @@
+import util.error.Logger;
+
+/**
+ * Created by Administrator on 16-7-30.
+ */
+public class LogSystem {
+    public static void main(String[] args) {
+        Logger.log("lalala");
+    }
+}
