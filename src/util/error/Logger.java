1c0466e9822b50c3eeaf8fa0203d445888dc4b01
Eldath <lizhaohan001@live.cn>
2016-08-19 20:53:54 +0800

    。。算了吧。我就是随便提交下而已。。

--- src/util/error/Logger.java
+++ src/util/error/Logger.java
@@ -1,5 +1,8 @@
 package util.error;
 
+import view.GUI;
+
+import javax.swing.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,50 +14,57 @@ import java.time.LocalDateTime;
  */
 public class Logger {
 
-    private static final String template = "log" + File.separator + "error%d.log";
-    private static FileWriter writer;
-    private static File file;
+	private static final String template = "log" + File.separator + "error%d.log";
+	private static FileWriter writer;
+	private static File file;
 
-    public static void log(String log) {
-        init();
-        try {
-            writer
-                    .append(LocalDateTime.now().toString())
-                    .append('\n')
-                    .append(log)
-                    .append('\n')
-                    .close();
-        } catch (IOException ignored) {
-        }
-    }
+	public static void log(String log) {
+		init();
+		try {
+			writer
+					.append(LocalDateTime.now().toString())
+					.append('\n')
+					.append(log)
+					.append('\n')
+					.close();
+		} catch (IOException ignored) {
+		}
+	}
 
-    public static void log(Exception e) {
-        init();
-        try {
-            writer.append(LocalDateTime.now().toString()).append('\n');
-            for (StackTraceElement element : e.getStackTrace())
-                writer.append(element.toString()).append('\n');
-            writer.flush();
-            writer.close();
-        } catch (IOException ignored) {
-        }
-    }
+	public static void log(Exception e) {
+		init();
+		try {
+			writer.append(LocalDateTime.now().toString()).append('\n');
+			for (StackTraceElement element : e.getStackTrace())
+				writer.append(element.toString()).append('\n');
+			writer.flush();
+			writer.close();
+		} catch (IOException ignored) {
+		}
+	}
 
-    private static void init() {
-        try {
-            File path = new File("log");
-            if (!path.exists())
-                path.mkdir();
-            //历史遗留问题。。【滑稽滑稽滑稽】
-            for (int i = 0; ; i++) {
-                file = new File(String.format(template, i));
-                if (!file.exists()) {
-                    file.createNewFile();
-                    break;
-                }
-            }
-            writer = new FileWriter(file);
-        } catch (Exception ignored) {
-        }
-    }
+	private static void init() {
+		try {
+			File path = new File("log");
+			if (!path.exists())
+				path.mkdir();
+			for (int i = 0; ; i++) {
+				file = new File(String.format(template, i));
+				if (!file.exists()) {
+					file.createNewFile();
+					break;
+				}
+			}
+			writer = new FileWriter(file);
+		} catch (Exception ignored) {
+			if (JOptionPane.showConfirmDialog(
+					GUI.getMainFrame(),
+					"致命错误",
+					"日志系统初始化失败：\r\n" + ignored.getMessage(),
+					JOptionPane.OK_OPTION
+			) == JOptionPane.OK_OPTION) {
+				System.exit(0);
+			}
+		}
+	}
 }
